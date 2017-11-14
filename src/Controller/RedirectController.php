8934045e988a4c36b8a5235df16f9a2c1a0c3af6
mhavelant <mhavelant@brainsum.com>
2017-11-14 12:11:41 +0100

    CS fixes, refactor controller to extend ControllerBase and use dependency injection.
    	- redirect() has been renamed to redirectHandler() to avoid collisions with ControllerBase.

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -2,12 +2,70 @@
 
 namespace Drupal\client_support\Controller;
 
+use Drupal\client_support\Component\SupportIntegrationManager;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\DependencyInjection\ClassResolverInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Class RedirectController.
  *
  * @package Drupal\client_support\Controller
  */
-class RedirectController {
+class RedirectController extends ControllerBase {
+
+  /**
+   * The plugin manager for the Client Support module.
+   *
+   * @var \Drupal\client_support\Component\SupportIntegrationManager
+   */
+  protected $supportPluginManager;
+
+  /**
+   * Config for the module.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $supportSettings;
+
+  /**
+   * Class resolver service.
+   *
+   * @var \Drupal\Core\DependencyInjection\ClassResolverInterface
+   */
+  protected $classResolver;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('plugin.manager.support_integration'),
+      $container->get('class_resolver')
+    );
+  }
+
+  /**
+   * RedirectController constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory.
+   * @param \Drupal\client_support\Component\SupportIntegrationManager $supportPluginManager
+   *   The plugin manager for the Client Support module.
+   * @param \Drupal\Core\DependencyInjection\ClassResolverInterface $classResolver
+   *   Class resolver service.
+   */
+  public function __construct(
+    ConfigFactoryInterface $configFactory,
+    SupportIntegrationManager $supportPluginManager,
+    ClassResolverInterface $classResolver
+  ) {
+    $this->supportPluginManager = $supportPluginManager;
+    $this->supportSettings = $configFactory->get('client_support.settings');
+    $this->classResolver = $classResolver;
+  }
 
   /**
    * Redirect handler.
@@ -17,16 +75,13 @@ class RedirectController {
    *
    * @throws \InvalidArgumentException
    */
-  public function redirect() {
-    // @todo: Dependency injection.
-    /** @var \Drupal\client_support\Component\SupportIntegrationManager $manager */
-    $manager = \Drupal::service('plugin.manager.support_integration');
-    $plugins = $manager->getDefinitions();
+  public function redirectHandler() {
+    $plugins = $this->supportPluginManager->getDefinitions();
 
-    $pluginId = \Drupal::configFactory()->get('client_support.settings')->get('settings.integration_plugin');
+    $pluginId = $this->supportSettings->get('settings.integration_plugin');
 
     /** @var \Drupal\client_support\Component\SupportIntegrationInterface $pluginInstance */
-    $pluginInstance = \Drupal::service('class_resolver')
+    $pluginInstance = $this->classResolver
       ->getInstanceFromDefinition($plugins[$pluginId]['class']);
 
     return $pluginInstance->redirect();
